fc47e3e9ec593180415f6cf452928f334c5d471a
ItsMeSamey <sameychain5041@gmail.com>
2025-03-11 10:37:59 +0530

    updated gen.zig

--- backend/game/text_gen/gen.zig
+++ backend/game/text_gen/gen.zig
@@ -5,12 +5,22 @@ const std = @import("std");
 
 const StringStruct = extern struct {
   ptr: [*]const c_char,
-  len: c_int,
+  len: u32,
+  capacity: u32,
 
-  pub fn formSlice(slice: []const u8) @This() {
+  pub fn toArrayList(self: @This()) std.ArrayListUnmanaged(u8) {
     return .{
-      .ptr = @ptrCast(slice.ptr),
-      .len = @intCast(slice.len),
+      .items = self.ptr[0..self.len],
+      .capacity = self.capacity,
+    };
+  }
+
+  pub fn fromArrayList(list: std.ArrayListUnmanaged(u8)) @This() {
+    if (list.capacity > std.math.maxInt(u32)) list.shrinkAndFree(gpa.allocator(), std.math.maxInt(u32));
+    return .{
+      .ptr = list.items.ptr,
+      .len = @intCast(list.items.len),
+      .capacity = @intCast(list.capacity),
     };
   }
 };
@@ -22,36 +32,64 @@ const GenSentence = GenWords(.{.defaultData = @embedFile("./text_gen/src/data/wo
 
 const GenMarkov = @import("text_gen/src/genMarkov.zig");
 
+// const TypeInt = enum(u2) {
+//   WordAlpha = 0,
+//   WordNonAlpha = 1,
+//   Sentence = 2,
+//   Markov = 3,
+//
+//   pub fn fromType(comptime T: type) TypeInt {
+//     return switch (T) {
+//       GenWordAlpha => .WordAlpha,
+//       GenWordNonAlpha => .WordNonAlpha,
+//       GenSentence => .Sentence,
+//       GenMarkov.AnyMarkovGen => .Markov,
+//       else => @compileError("Invalid type"),
+//     };
+//   }
+//
+//   pub fn toType(comptime self: TypeInt) type {
+//     return switch (self) {
+//       .WordAlpha => GenWordAlpha,
+//       .WordNonAlpha => GenWordNonAlpha,
+//       .Sentence => GenSentence,
+//       .Markov => GenMarkov.AnyMarkovGen,
+//     };
+//   }
+// };
+
 var char_markov: GenMarkov.AnyMarkovGen = undefined;
 var word_markov: GenMarkov.AnyMarkovGen = undefined;
 
-fn getWordAlpha() GenWordAlpha { return .{.state = .{.random = newRandom()}, .data = .{}}; }
-fn getWordNonAlpha() GenWordNonAlpha { return .{.state = .{}, .data = .{}}; }
-fn getSentence() GenSentence { return .{.state = .{}, .data = .{}}; }
-fn getMarkovChar() GenMarkov.AnyMarkovGen { return char_markov.dupe(); }
-fn getMarkovWord() GenMarkov.AnyMarkovGen { return word_markov.dupe(); }
-
-fn newRandom() std.Random {
-  return std.Random.DefaultPrng.init(0);
-}
-
-var gpa: std.heap.GeneralPurposeAllocator(.{}) = .{};
+var gpa: std.heap.GeneralPurposeAllocator(.{.thread_safe = true}) = .{};
+var globalRng: std.Random.DefaultPrng = undefined;
 
 /// This must be called before any other function in this library,
 /// Calls to any other function before calling init are UB
 pub export fn init() void {
+  const seeds: [2]u64 = @bitCast(std.time.nanoTimestamp());
+  globalRng = std.Random.DefaultPrng.init(seeds[0]^seeds[1]);
+
   const allocator = gpa.allocator();
 
   // Since models are generated on the same machine, endianness will be the same, so no need for copyable init
-  char_markov = GenMarkov.initImmutableUncopyable(@embedFile("./text_gen/src/data/markov.char"), allocator)
-    catch |e| std.debug.panic("Error: {!}", .{ e });
-  word_markov = GenMarkov.initImmutableUncopyable(@embedFile("./text_gen/src/data/markov.word"), allocator)
-    catch |e| std.debug.panic("Error: {!}", .{ e });
+  char_markov = GenMarkov.initImmutableUncopyable(@embedFile("./text_gen/src/data/markov.char"), .{
+    .allocator = allocator,
+    .random = newRandom(),
+  }) catch |e| std.debug.panic("Error: {!}", .{ e });
+
+  word_markov = GenMarkov.initImmutableUncopyable(@embedFile("./text_gen/src/data/markov.word"), .{
+    .allocator = allocator,
+    .random = newRandom(),
+  }) catch |e| std.debug.panic("Error: {!}", .{ e });
 }
 
 /// It is unsafe to call this function if init is not called;
 pub export fn deinit() void {
-  word_markov.free();
+  const allocator = gpa.allocator();
+
+  char_markov.free(allocator);
+  word_markov.free(allocator);
 
   const check = gpa.deinit();
   if (check != .ok) std.debug.panic("Error: memory leak detected", .{});
@@ -59,15 +97,58 @@ pub export fn deinit() void {
   gpa = .{};
 }
 
-// -> word_markov specific functions
-
-/// Generate a random word
-pub export fn genWordMarkov() string {
-  return sliceToString(word_markov.gen());
+/// New seeded rng (we really dont care about race conditions here!!)
+fn newRandom() std.Random {
+  return globalRng.random();
 }
 
-/// Re-roll the markov generator
-pub export fn rollWordMarkov() void {
-  word_markov.roll();
+// fn create(generator: anytype) *anyopaque {
+//   const alignment = @max(@alignOf(generator), 1 << @bitSizeOf(TypeInt));
+//   const mem = gpa.allocator().alignedAlloc(@TypeOf(generator), alignment, @sizeOf(generator)) catch @panic("OOM");
+//
+//   const newGenerator: *@TypeOf(generator) = @ptrCast(mem.ptr);
+//   newGenerator.roll();
+//
+//   @memcpy(newGenerator, &generator);
+//   return @ptrFromInt(@intFromPtr(newGenerator) | TypeInt.fromType(@TypeOf(generator)));
+// }
+//
+// pub export fn getWordAlpha() *anyopaque { return create(GenWordAlpha{.state = .{.random = newRandom()}, .data = .{}}); }
+// pub export fn getWordNonAlpha() *anyopaque { return create(GenWordNonAlpha{.state = .{.random = newRandom()}, .data = .{}}); }
+// pub export fn getSentence() *anyopaque { return create(GenSentence{.state = .{.random = newRandom()}, .data = .{}}); }
+// pub export fn getMarkovChar() *anyopaque { return create(char_markov.dupe(gpa.allocator())); }
+// pub export fn getMarkovWord() *anyopaque { return create(word_markov.dupe(gpa.allocator())); }
+
+pub export fn genN(state: u32, n: u16, id: u8) StringStruct {
+  const allocator = gpa.allocator();
+  if (n == 0) return .{.ptr = undefined, .len = 0, .capacity = 0};
+
+  switch (id) {
+    inline 0, 1, 2, 3, 4 => |comptime_id| {
+      const generator = switch (comptime_id) {
+        0 => GenWordAlpha{.state = .{.random = newRandom()}, .data = .{}},
+        1 => GenWordNonAlpha{.state = .{.random = newRandom()}, .data = .{}},
+        2 => GenSentence{.state = .{.random = newRandom()}, .data = .{}},
+        3 => char_markov.dupe(allocator),
+        4 => word_markov.dupe(allocator),
+      };
+      if (state != std.math.maxInt(u32)) generator.state().at = state;
+      var array_list = std.ArrayListUnmanaged(u8){};
+
+      for (0..n-1) |_| {
+        const word = generator.gen();
+        array_list.ensureUnusedCapacity(word.len + 1) catch @panic("OOM");
+        array_list.appendSliceAssumeCapacity(word);
+        array_list.appendAssumeCapacity(' ');
+      }
+      array_list.appendSlice(generator.gen()) catch @panic("OOM");
+
+      return StringStruct.fromArrayList(array_list);
+    }
+  }
+}
+
+pub export fn free(string: StringStruct) void {
+  gpa.allocator().free(string.ptr[0..string.len]);
 }
 
